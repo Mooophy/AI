b7d7dbe2bc830468078e6a462c2a4457eeb57fab
moophy <MoophyAlan@gmail.com>
2015-03-13 01:07:12 +1300

    setup unit test

---
+++ src/search/unit_test/test_node.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace unit_test
+{		
+	TEST_CLASS(TestNode)
+	{
+	public:
+		
+		TEST_METHOD(ctor)
+		{
+
+		}
+
+	};
+}
