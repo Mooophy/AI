61dc5dfb0d7abb7b515e729da0b6790820a0234a
moophy <MoophyAlan@gmail.com>
2015-03-13 17:32:19 +1300

    action map tested 	modified:   src/search/unit_test/test_action_map.cpp

--- src/search/unit_test/test_action_map.cpp
+++ src/search/unit_test/test_action_map.cpp
@@ -51,7 +51,6 @@ namespace unit_test
 			ai::search::ActionMap const actions;
 
 			Assert::AreEqual(9u, actions.size());
-			
 			Assert::AreEqual(2u, actions.at(0).size());
 			Assert::AreEqual(3u, actions.at(1).size());
 			Assert::AreEqual(2u, actions.at(2).size());
@@ -61,13 +60,21 @@ namespace unit_test
 			Assert::AreEqual(2u, actions.at(6).size());
 			Assert::AreEqual(3u, actions.at(7).size());
 			Assert::AreEqual(2u, actions.at(8).size());
+		}
 
-
+		TEST_METHOD(subscript)
+		{
+			ai::search::ActionMap const actions;
 			std::vector<ai::search::Node> children;
 			ai::search::Node parent("876543210", "");
-			for (auto& make_child : actions.at(8))
+			for (auto make_child : actions.at(8))
 				children.push_back(make_child(parent));
+
 			Assert::AreEqual(2u, children.size());
+			Assert::IsTrue("876540213" == children[0].state);
+			Assert::IsTrue("876543201" == children[1].state);
+			Assert::IsTrue("U" == children[0].path);
+			Assert::IsTrue("L" == children[1].path);
 		}
 	};
 }
