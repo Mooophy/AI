c4996d6c920ac4df017c8730c1b95ff6fda30c92
moophy <MoophyAlan@gmail.com>
2015-03-15 21:35:23 +1300

    implemented best first search, not tested yet

--- src/search/search/best_first_search.hpp
+++ src/search/search/best_first_search.hpp
@@ -1,7 +1,10 @@
 #pragma once 
 
 #include <string>
+#include <queue>
+#include <set>
 #include "node.h"
+#include "action_map.h"
 
 namespace ai
 {
@@ -39,6 +42,60 @@ namespace ai
 
 			const std::string goal;
 			const HeuristicFunc heuristic;
+		};
+
+		template<typename HeuristicFunc>
+		class BestFirstSearch
+		{
+		public:
+			using MinPriorityQueue = std::priority_queue < Node, std::vector<Node>, GreaterThan<HeuristicFunc> >;
+
+			BestFirstSearch(std::string const& source, std::string const& goal): 
+				visited_set_{},
+				pq_{},
+				path_to_goal_{},
+				action_dictionary{}
+			{
+				for (pq_.push(Node(source, ""));	!pq_.empty();	/* */)
+				{
+					auto curr = pq_.top();	pq_.pop();
+					visited_set_.insert(curr.state);
+					
+					if (goal == curr.state)
+					{
+						path_to_goal_ = curr.path;
+						break;
+					}
+
+					for (auto make_child : action_dictionary.at(curr.state.find('0')))
+					{
+						auto child = make_child(curr);
+						if (visited_set_.end() == visited_set_.find(child.state))
+							pq_.push(child);
+					}
+				}
+			}
+
+			std::set<std::string> const& visited_set() const
+			{
+				return visited_set_;
+			}
+			MinPriorityQueue const& queue() const
+			{
+				return queue_;
+			}
+			std::string const& path_to_goal() const
+			{
+				return path_to_goal_;
+			}
+
+		private:
+			std::set<std::string> visited_set_;
+			MinPriorityQueue pq_;
+			std::string path_to_goal_;
+
+		public:
+			const ActionMap action_dictionary;
 		};
 	}
 }
