28e27f59072fb736dacb08704765754f304d76c5
moophy <MoophyAlan@gmail.com>
2015-03-18 20:31:19 +1300

    add h function : misplaced tiles, tested

--- src/search/unit_test/test_heuristic_func.cpp
+++ src/search/unit_test/test_heuristic_func.cpp
@@ -17,5 +17,19 @@ namespace unit_test
 			Assert::AreEqual(18u, distance);
 		}
 
+		TEST_METHOD(misplaced_tiles)
+		{
+			ai::search::MisplacedTiles h;
+
+			auto v1 = h("724506831", "012345678");
+			Assert::AreEqual(9u, v1);
+
+			auto v2 = h("012345678", "012345678");
+			Assert::AreEqual(0u, v2);
+
+			auto v3 = h("812345670", "012345678");
+			Assert::AreEqual(2u, v3);
+		}
+
 	};
 }
