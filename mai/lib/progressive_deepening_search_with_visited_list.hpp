dd3ca4e9482a815a32c27a6c23ce1e600236e41b
moophy <MoophyAlan@gmail.com>
2015-04-26 00:13:28 +1200

    ready to test :   mai/lib/progressive_deepening_search_with_visited_list.hpp

---
+++ mai/lib/progressive_deepening_search_with_visited_list.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <set>
+#include <string>
+#include <deque>
+#include <algorithm>
+#include <chrono>
+#include "node.hpp"
+#include "function_dictionary.hpp"
+
+namespace mai
+{
+    namespace search
+    {
+        class ProgressiveDeepeningSearchWithVisitedList
+        {
+            using Time = std::chrono::high_resolution_clock;
+        public:
+            ProgressiveDeepeningSearchWithVisitedList(std::string const& source, std::string const& goal) :
+                max_depth_{0}, max_q_length_{0},
+                visited_{},
+                q_{},
+                final_path_{},
+                running_time_{0},
+                func_dic_{}
+            {
+                auto start = Time::now();
+
+                for (max_depth_ = 0; /* true */; ++max_depth_)
+                {
+                    visited_.clear();
+                    q_.clear();
+                    for (q_.push_front(Node(source, "")); !q_.empty(); /* */)
+                    {
+                        auto curr = q_.front();	q_.pop_front();
+                        visited_.insert(curr.state);
+
+                        if (goal == curr.state){ final_path_ = curr.path; return; }
+                        if (curr.path.size() >= max_depth_) continue;
+
+                        for (auto make_child : func_dic_.at(curr.state.find('0')))
+                        {
+                            auto child = make_child(curr);
+                            if (visited_.end() == visited_.find(child.state)) q_.push_front(child);
+                        }
+
+                        if (q_.size() > max_q_length_) max_q_length_ = q_.size();
+                    }
+                }
+
+                auto done = Time::now();
+                running_time_ = std::chrono::duration<float>(done - start).count();
+            }
+        private:
+            std::size_t max_depth_, max_q_length_;
+            std::set<std::string> visited_;
+            std::deque < mai::search::Node > q_;
+            std::string final_path_;
+            float running_time_;
+            const mai::search::FunctionDictionary func_dic_;
+        };
+    }
+}
