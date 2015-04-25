55a9db0f54834d841c00a5d9a9c0a7f927b17b65
moophy <MoophyAlan@gmail.com>
2015-04-26 00:52:38 +1200

    modified:   mai/unit_tests/test_pds_with_vlist.cpp

--- mai/unit_tests/test_pds_with_vlist.cpp
+++ mai/unit_tests/test_pds_with_vlist.cpp
@@ -6,16 +6,16 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace unit_tests
 {
-	TEST_CLASS(test_pds_with_vlist)
-	{
-	public:
-		
-		TEST_METHOD(pds_ctor)
-		{
+    TEST_CLASS(test_pds_with_vlist)
+    {
+    public:
+
+        TEST_METHOD(pds_ctor)
+        {
             auto pds = mai::search::ProgressiveDeepeningSearchWithVisitedList("102345678", "012345678");
             Assert::AreEqual(std::string{ "L" }, pds.path());
             Assert::AreEqual(3u, pds.visited().size());
-		}
+        }
 
         //12.5s
         TEST_METHOD(pds_876543210)
@@ -30,5 +30,5 @@ namespace unit_tests
             Assert::IsTrue(12.0f < pds.running_time() && 15.0f > pds.running_time());
         }
 
-	};
+    };
 }
