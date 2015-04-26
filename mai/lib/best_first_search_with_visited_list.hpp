2762eb1105d97670c6ce52b546b3a6328133688e
moophy <MoophyAlan@gmail.com>
2015-04-26 18:27:56 +1200

    new file:   unit_tests/test_bestfs_with_vlist.cpp

--- mai/lib/best_first_search_with_visited_list.hpp
+++ mai/lib/best_first_search_with_visited_list.hpp
@@ -17,22 +17,20 @@ namespace mai
         {
             struct Greater
             {
-                Greater(HeuristicFunc h, std::string const& g) : goal(g), heuristic(h){}
-                bool operator()(Node const& lhs, Node const& rhs) const
-                {
-                    return heuristic(lhs.state, goal) > heuristic(rhs.state, goal);
-                }
+                Greater(std::string const& g) : goal(g), h{}{}
+                bool operator()(Node const& lhs, Node const& rhs) const { return h(lhs.state, goal) > h(rhs.state, goal); }
 
                 const std::string goal;
-                const HeuristicFunc heuristic;
+                const HeuristicFunc h;
             };
 
-            using MinPriorityQueue = std::priority_queue < Node, std::vector<Node>, GreaterThan<HeuristicFunc> > ;
+            using MinPriorityQueue = std::priority_queue < Node, std::vector<Node>, Greater > ;
+            using Time = std::chrono::high_resolution_clock;
         public:
             BestFSWithVList(std::string const& source, std::string const& goal) :
                 max_q_length_{ 0u },
                 visited_{},
-                pq_{ Greater{} },
+                pq_{ Greater{ goal } },
                 final_path_{},
                 running_time_{ 0.0f },
                 func_dic_{}
@@ -44,18 +42,26 @@ namespace mai
                     auto curr = pq_.top();	pq_.pop();
                     visited_.insert(curr.state);
 
-                    if (goal == curr.state){ path_to_goal_ = curr.path; goto Done; }
-                    for (auto make_child : action_dictionary.at(curr.state.find('0')))
+                    if (goal == curr.state){ final_path_ = curr.path; goto Done; }
+
+                    for (auto make_child : func_dic_.at(curr.state.find('0')))
                     {
                         auto child = make_child(curr);
                         if (visited_.end() == visited_.find(child.state))   pq_.push(child);
                     }
+
+                    if (pq_.size() > max_q_length_) max_q_length_ = pq_.size();
                 }
 
             Done:
                 auto done = Time::now();
                 running_time_ = std::chrono::duration<float>(done - start).count();
             }
+
+            std::size_t max_q_length() const { return max_q_length_; }
+            std::set<std::string> const& visited() const { return visited_; }
+            std::string const& path() const { return final_path_; }
+            float running_time() const { return running_time_; }
 
         private:
             std::size_t max_q_length_;
---
+++ mai/unit_tests/test_bestfs_with_vlist.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../lib/best_first_search_with_visited_list.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace unit_tests
+{
+	TEST_CLASS(test_bestfs_with_vlist)
+	{
+	public:
+		
+		TEST_METHOD(bestfs_ctor)
+		{
+            auto bestfs = mai::search::BestFSWithVList < mai::search::ManhattanDistance > {"876543210", "012345678"};
+            Assert::AreEqual(std::string("UULLDDRRUULLDDRRUULLDDRRUULL"), bestfs.path());
+            Assert::AreEqual(35u, bestfs.visited().size());
+            Assert::AreEqual(19u, bestfs.max_q_length());
+            Assert::IsTrue(0.001f < bestfs.running_time() && 1.0f > bestfs.running_time());
+		}
+
+	};
+}
