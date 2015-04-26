b1a5b3c8fd91c783a50ac8c7cb09132fbc8393b2
moophy <MoophyAlan@gmail.com>
2015-04-26 18:29:13 +1200

    modified:   unit_tests/test_bestfs_with_vlist.cpp

--- mai/unit_tests/test_bestfs_with_vlist.cpp
+++ mai/unit_tests/test_bestfs_with_vlist.cpp
@@ -6,18 +6,18 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace unit_tests
 {
-	TEST_CLASS(test_bestfs_with_vlist)
-	{
-	public:
-		
-		TEST_METHOD(bestfs_ctor)
-		{
+    TEST_CLASS(test_bestfs_with_vlist)
+    {
+    public:
+
+        TEST_METHOD(bestfs_ctor)
+        {
             auto bestfs = mai::search::BestFSWithVList < mai::search::ManhattanDistance > {"876543210", "012345678"};
             Assert::AreEqual(std::string("UULLDDRRUULLDDRRUULLDDRRUULL"), bestfs.path());
             Assert::AreEqual(35u, bestfs.visited().size());
             Assert::AreEqual(19u, bestfs.max_q_length());
             Assert::IsTrue(0.001f < bestfs.running_time() && 1.0f > bestfs.running_time());
-		}
+        }
 
-	};
+    };
 }
