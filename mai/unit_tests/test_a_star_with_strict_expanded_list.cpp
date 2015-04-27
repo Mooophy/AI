1edfb2f7bc22d04484c800fffa2356de45ed1a44
moophy <MoophyAlan@gmail.com>
2015-04-27 21:34:56 +1200

    format 	modified:   mai/unit_tests/test_a_star_with_strict_expanded_list.cpp

--- mai/unit_tests/test_a_star_with_strict_expanded_list.cpp
+++ mai/unit_tests/test_a_star_with_strict_expanded_list.cpp
@@ -7,20 +7,20 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace unit_tests
 {
-	TEST_CLASS(test_a_star_with_strict_expanded_list)
-	{
-	public:
-		
+    TEST_CLASS(test_a_star_with_strict_expanded_list)
+    {
+    public:
+
         // < 0.001s
         TEST_METHOD(a_star_sel_with_ManhattanDistance_876543210)
-		{
+        {
             auto astar_sel = mai::search::AStarSEL < mai::search::ManhattanDistance > { "876543210", "012345678" };
             std::string expect_path = "LLUURRDDLLUURRDDLLUURRDDLLUU";
 
             Assert::AreEqual(expect_path, astar_sel.path());
             Assert::AreEqual(265u, astar_sel.expanded().size());
             Assert::AreEqual(151u, astar_sel.max_q_length());
-		}
+        }
 
         // 5s
         TEST_METHOD(a_star_sel_with_MisplacedTiles_876543210)
@@ -33,5 +33,5 @@ namespace unit_tests
             Assert::AreEqual(20411u, astar_sel.max_q_length());
         }
 
-	};
+    };
 }
