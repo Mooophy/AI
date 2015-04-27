35110c2d3aa26286c97b45d1b08cc33b9a6bdba4
Mooophy <moophyalan@gmail.com>
2015-04-28 11:58:46 +1200

    new file:   mai/lib/time_recorder.hpp

---
+++ mai/lib/time_recorder.hpp
@@ -0,0 +1,27 @@
+#pragma once 
+#include <chrono>
+
+namespace mai
+{
+	namespace utility
+	{
+		class TimeRecorder
+		{
+		public:
+			using Time = std::chrono::high_resolution_clock;
+			using TimePoint = std::chrono::high_resolution_clock::time_point;
+
+			explicit TimeRecorder(float& dura) :
+				duration_{ dura }, start_{ Time::now() }, done_{ }
+			{ }
+
+			~TimeRecorder()
+			{
+				duration_ = std::chrono::duration<float>((done_ = Time::now()) - start_).count();
+			}
+		private:
+			float& duration_;
+			TimePoint start_, done_;
+		};
+	}
+}
