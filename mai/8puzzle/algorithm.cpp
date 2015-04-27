041075edb31bfcbc4d77667dbd25bb5abce88043
moophy <MoophyAlan@gmail.com>
2015-04-27 21:35:37 +1200

    link astar_sel with api

--- mai/8puzzle/algorithm.cpp
+++ mai/8puzzle/algorithm.cpp
@@ -1,9 +1,10 @@
-
 #include "algorithm.h"
 #include "../lib/progressive_deepening_search_with_visited_list.hpp"
 #include "../lib/best_first_search_with_visited_list.hpp"
 #include "../lib/uniform_cost_search.hpp"
 #include "../lib/a_star.hpp"
+#include "../lib/a_star_with_strict_expanded_list.hpp"
+
 using namespace std;
 
 
@@ -50,8 +51,6 @@ string uniformCost_Exp_List(string const initialState, string const goalState, i
     return ucs.path();
 }
 
-
-
 ///////////////////////////////////////////////////////////////////////////////////////////
 //
 // Search Algorithm:  A* without the ExpandedList
@@ -77,44 +76,27 @@ string aStar(string const initialState, string const goalState, int &numOfStateE
     }
 }
 
-
-
 ///////////////////////////////////////////////////////////////////////////////////////////
 //
 // Search Algorithm:  A* using the Strict ExpandedList
 //
 ////////////////////////////////////////////////////////////////////////////////////////////
-string aStar_Exp_List(string const initialState, string const goalState, int &numOfStateExpansions, int& maxQLength, float &actualRunningTime, heuristicFunction heuristic){
-    string path;
-    clock_t startTime;
-
-    startTime = clock();
-
-    //***********************************************************************************************************
-    // BEGIN_DUMMY_CODES - YOU SHOULD DELETE THIS BLOCK OF CODES AND REPLACE WITH YOUR ALGORITHM IMPLEMENTATION 
-    //
-    // run search algorithm here
-    //
-    //
-    numOfStateExpansions = 777; //this is for testing only
-    maxQLength = 333; //this is for testing only
-
-
-    long i, j;
-
-    j = 0;
-    while (j < 99999){
-        j++;
-        i = 0;
-        while (i < 99){
-            i++;
-        }
+string aStar_Exp_List(string const initialState, string const goalState, int &numOfStateExpansions, int& maxQLength, float &actualRunningTime, heuristicFunction heuristic)
+{
+    if (heuristic == manhattanDistance)
+    {
+        auto astar_sel = mai::search::AStarSEL < mai::search::ManhattanDistance > { initialState, goalState };
+        numOfStateExpansions = astar_sel.expanded().size();
+        maxQLength = astar_sel.max_q_length();
+        actualRunningTime = astar_sel.running_time();
+        return astar_sel.path();
     }
-    path = "LUULD";
-    //
-    // END_DUMMY_CODES
-    //***********************************************************************************************************
-    actualRunningTime = ((float)(clock() - startTime) / CLOCKS_PER_SEC);
-
-    return path;
+    else
+    {
+        auto astar_sel = mai::search::AStarSEL < mai::search::MisplacedTiles > { initialState, goalState };
+        numOfStateExpansions = astar_sel.expanded().size();
+        maxQLength = astar_sel.max_q_length();
+        actualRunningTime = astar_sel.running_time();
+        return astar_sel.path();
+    }
 }
