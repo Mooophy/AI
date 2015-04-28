427e93cbbdc14925440e1cf91b65fd172eb08336
Mooophy <moophyalan@gmail.com>
2015-04-28 18:56:02 +1200

    modified:   mai/lib/a_star.hpp

--- mai/lib/a_star.hpp
+++ mai/lib/a_star.hpp
@@ -39,7 +39,7 @@ namespace mai
                 : expansions_{ 0 }, max_q_length_{ 0 }, q_{ Greater{ goal } }, final_path_{}, running_time_{ 0.0f }, func_dic_{}
             {
                 auto timing = mai::utility::TimeRecord{ running_time_ };
-                std::tie( final_path_, expansions_ ) = search( source, goal );
+                search( source, goal );
             }
 
             auto max_q_length() const -> std::size_t { return max_q_length_; }
@@ -54,18 +54,15 @@ namespace mai
             float running_time_;
             const mai::search::FunctionDictionary func_dic_;
 
-            auto search(std::string const& source, std::string const goal) const -> std::tuple< std::string, std::size_t >
+            auto search(std::string const& source, std::string const goal) -> void
             {
-                auto num_of_expansions = std::size_t{ 0 };
-                auto path = std::string{};
-                for (q_.push(Node{ source, "" }); !q_.empty(); ++num_of_expansions)
+                for (q_.push(Node{ source, "" }); !q_.empty(); ++expansions_)
                 {
                     auto curr = q_.top(); q_.pop();
-                    if (curr.state == goal){ path = curr.path; break; }
+                    if (curr.state == goal){ final_path_ = curr.path; break; }
                     for (auto make_child : func_dic_.at(curr.state.find('0'))) q_.push(make_child(curr));
                     max_q_length_ = std::max(max_q_length_, q_.size());
                 }
-                return std::make_tuple( path, num_of_expansions );
             }
         };
     }
